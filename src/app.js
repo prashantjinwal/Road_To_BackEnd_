fb0cf3abfd9cd0cdc8bd2b96d40a2d7f7a289a75
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-13 22:24:14 +0530

    DB connection improved

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+import cros from "cors"
+import cookieParser from "cookie-parser";
+
+
+const app = express();
+
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 // require('dotenv').config({path: "./env"})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
 
-connectDB();
-
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running at ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("DataBase connection failed !!", err)
+})
 
 
 
