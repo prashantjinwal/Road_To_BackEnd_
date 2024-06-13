01cf45bf7e979fe2a71bc2e2bea2afd654e04dc0
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-13 22:03:18 +0530

    DB connection

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 # Road to backend 
 
 - [Model Link](https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj)
+
+ node version 19.3.0 < is require (because i gonna use "--experimental-json-module", resolve futher)  
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+// require('dotenv').config({path: "./env"})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
 
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB();
+
+
+
+
+
+/*
+    import express from "express"
+    const app = express()
 
 ( async () =>{
     try{
        await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME} `)
+       app.on("errror",(error)=>{
+        console.log("ERRR", error)
+        throw error
+       })
+       app.listen(prrocess.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT} `)
+       })
     }catch(error){
         console.error("ERROR", error);
         throw err;
     }
 })()
+
+*/
